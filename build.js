e4a7ca41619238c38a52bae33eab30eea4421fe0
Tom A. Vibeto <tom@onlyhuman.dk>
2018-06-11 02:51:22 +0200

    MMIP ini files don't support quotation marks

--- build.js
+++ build.js
@@ -1,32 +1,22 @@
 const ini = require('multi-ini');
 const zipdir = require('zip-dir');
-const semver = require('semver');
-const packagejson = require('./package.json');
 
 let fullverstring = '';
-const version = packagejson.version;
 const file = './source/Install.ini';
 const content = ini.read(file);
 
 if (
 	content &&
 	content.Header &&
-	content.Header.VersionBuild
+	content.Header.VersionMajor
 ) {
 	const head = content.Header;
-
-	head.VersionMajor = `${semver(version).major}`;
-	head.VersionMinor = `${semver(version).minor}`;
-	head.VersionRelease = `${semver(version).patch}`;
-	head.VersionBuild = `${Number(head.VersionBuild) + 1}`;
 
 	fullverstring = [
 		head.VersionMajor, 
 		head.VersionMinor, 
-		head.VersionRelease, 
-		head.VersionBuild
+		head.VersionRelease
 	].join('.');
-	ini.write(file, content);
 }
 zipdir('./source', {
 	saveTo: `./dist/Lyrics To Instrumental v${fullverstring}.mmip`
